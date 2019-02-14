d0e2e86e0a519f451b65967ecf103130568c20cf
Giasemi Seisa <giasemi.seisa@endocode.com>
2019-02-14 21:41:39 +0100

    Extract library functionality from gradle plugin
    Keep only gradle parts in the plugin

--- src/main/java/org/qmstr/gradle/QmstrCompileTask.java
+++ src/main/java/org/qmstr/gradle/QmstrCompileTask.java
@@ -8,7 +8,16 @@ import org.qmstr.client.BuildServiceClient;
 import org.qmstr.grpc.service.Datamodel;
 import org.qmstr.util.FilenodeUtils;
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 public class QmstrCompileTask extends QmstrTask {
@@ -32,7 +41,7 @@ public class QmstrCompileTask extends QmstrTask {
             FileCollection sourceDirs = set.getAllJava().getSourceDirectories();
             SourceSetOutput outDirs = set.getOutput();
             set.getAllJava().forEach(js -> {
-                    Set<Datamodel.FileNode> nodes = FilenodeUtils.processSourceFile(js, sourceDirs, outDirs);
+                    Set<Datamodel.FileNode> nodes = processSourceFile(js, sourceDirs, outDirs);
                     if (!nodes.isEmpty()) {
                         bsc.SendBuildFileNodes(nodes);
                     }
@@ -41,7 +50,69 @@ public class QmstrCompileTask extends QmstrTask {
 
     }
 
+    public static Set<Datamodel.FileNode> processSourceFile(File sourcefile, FileCollection sourceDirs, FileCollection outDirs) {
 
+        Datamodel.FileNode sourceNode = FilenodeUtils.getFileNode(sourcefile.toPath(), FilenodeUtils.getTypeByFile(sourcefile.getName()));
 
+        Optional<File> actualSourceDir = sourceDirs
+                .filter(sd -> isActualSourceDir(sd, sourcefile))
+                .getFiles()
+                .stream()
+                .findFirst();
+
+        try {
+            Path relSrcPath = actualSourceDir.orElseThrow(FileNotFoundException::new).toPath().relativize(sourcefile.toPath());
+            String[] filename = relSrcPath.getFileName().toString().split("\\.");
+            filename[filename.length-1] = "class";
+            String className = String.join(".", filename);
+            Path packageDirs = relSrcPath.getParent();
+            Path classesRelPath = packageDirs.resolve(className);
+
+            if (packageDirs != null) {
+                return outDirs.filter(od -> isActualClassDir(od, classesRelPath)).getFiles().stream()
+                        .map(outdir -> {
+                            Path classesPath = outdir.toPath().resolve(classesRelPath);
+                            Set<Path> nested = getNestedClasses(outdir.toPath().resolve(packageDirs), filename[filename.length - 2]);
+                            nested.add(classesPath);
+                            return nested.stream()
+                                    .map(p -> FilenodeUtils.getFileNode(p, FilenodeUtils.getTypeByFile(p.getFileName().toString())))
+                                    .map(node -> node.toBuilder().addDerivedFrom(sourceNode).build())
+                                    .collect(Collectors.toSet());
+                        }).flatMap(sets -> sets.stream())
+                        .collect(Collectors.toSet());
+            }
+        } catch (FileNotFoundException fnfe) {
+            //TODO
+        }
+        return null;
+    }
+
+    private static boolean isActualSourceDir(File sourceDir, File sourceFile) {
+        return sourceFile.toString().startsWith(sourceDir.toString());
+    }
+
+    private static boolean isActualClassDir(File outdir, Path classesPath) {
+        return outdir.toPath().resolve(classesPath).toFile().exists();
+    }
+
+    private static Set<Path> getNestedClasses(Path dir, String outerclassname) {
+        try {
+            return Files.walk(dir)
+                    .filter(p -> isNestedClass(p, outerclassname))
+                    .collect(Collectors.toSet());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new HashSet<>();
+        }
+    }
+
+    private static boolean isNestedClass(Path classesPath, String outerClass) {
+        boolean file = classesPath.toFile().isFile();
+        String filename = classesPath.getFileName().toString();
+        boolean clazz = filename.endsWith(".class");
+        boolean starts = filename.startsWith(outerClass + "$");
+        return file && clazz && starts;
+    }
 
 }
--- src/main/java/org/qmstr/gradle/QmstrPackTask.java
+++ src/main/java/org/qmstr/gradle/QmstrPackTask.java
@@ -3,12 +3,17 @@ package org.qmstr.gradle;
 import org.gradle.api.artifacts.ConfigurationContainer;
 import org.gradle.api.artifacts.PublishArtifact;
 import org.gradle.api.tasks.TaskAction;
+import org.qmstr.grpc.service.Datamodel;
 import org.qmstr.client.BuildServiceClient;
 import org.qmstr.util.FilenodeUtils;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
+import java.util.jar.JarFile;
 import java.util.stream.Collectors;
 
 public class QmstrPackTask extends QmstrTask {
@@ -36,12 +41,43 @@ public class QmstrPackTask extends QmstrTask {
                 .forEach(c -> c.getAllArtifacts().forEach(art -> arts.put(art, c.getResolvedConfiguration().getFiles())));
 
         bsc.SendBuildFileNodes(arts.entrySet().parallelStream()
-                .map(artEntry -> FilenodeUtils.processArtifact(artEntry.getKey(), artEntry.getValue()))
+                .map(artEntry -> processArtifact(artEntry.getKey(), artEntry.getValue()))
                 .filter(o -> o.isPresent())
                 .map(o -> o.get())
                 .collect(Collectors.toSet())); 
         
     
+    }
+
+    public static Optional <Datamodel.FileNode> processArtifact(PublishArtifact artifact, Set<File> dependencySet) {
+        if (artifact.getExtension().equals("jar")) {
+            try {
+                Set<Datamodel.FileNode> classes = new HashSet<>();
+                JarFile jar = new JarFile(artifact.getFile());
+                jar.stream().parallel()
+                        .filter(je -> FilenodeUtils.isSupportedFile(je.getName()))
+                        .forEach(je -> {
+                            String hash = FilenodeUtils.getHash(jar, je);
+                            classes.add(FilenodeUtils.getFileNode(je.getName(), hash, FilenodeUtils.getTypeByFile(je.getName())));
+                        });
+                Datamodel.FileNode rootNode = FilenodeUtils.getFileNode(artifact.getFile().toPath(), FilenodeUtils.getTypeByFile(artifact.getFile().getName()));
+                Datamodel.FileNode.Builder rootNodeBuilder = rootNode.toBuilder();
+                classes.forEach(c -> rootNodeBuilder.addDerivedFrom(c));
+    
+                dependencySet.parallelStream()
+                        .map(f -> FilenodeUtils.getFileNode(f.toPath()))
+                        .filter(o -> o.isPresent())
+                        .map(o -> o.get())
+                        .forEach(depNode -> rootNodeBuilder.addDerivedFrom(depNode));
+    
+                rootNode = rootNodeBuilder.build();
+                return Optional.ofNullable(rootNode);
+    
+            } catch (IOException ioe) {
+                //TODO
+            }
+        }
+        return Optional.empty();
     }
 }
 
